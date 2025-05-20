8cdb87ecd6ae795e3bb4fde15ce0dab31e599215
Ryan Freitas <ryanvitorfreitas@gmail.com>
2025-05-20 16:19:06 -0300

    Changed

---
+++ Heranca_Polimorfismo/ENTREGAVEL/EXERCICIO1/models/veiculo.py
@@ -0,0 +1,20 @@
+class Veiculos:
+
+    def __init__(self, placa: str, modelo: str, marca: str, ano: int, cor: str, valor_fipe: float) -> None:
+        self.placa = placa
+        self.modelo = modelo
+        self.marca = marca
+        self.ano = ano
+        self.cor = cor
+        self.valor_fipe = valor_fipe
+
+    
+    def __str__(self):
+        infor = f"Placa: {self.placa}\n"
+        infor += f"Modelo: {self.modelo}\n"
+        infor += f"Marca: {self.marca}\n"
+        infor += f"Ano: {self.ano}\n"
+        infor += f"Cor: {self.cor}\n"
+        infor += f"Valor Fipe: {self.valor_fipe}\n"
+        return infor
+        
